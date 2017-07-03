48fce5e5b7f2ff7e892b037ea744fc79500bfc38
Jacob Khalili <jakekali@users.noreply.github.com>
2017-07-03 17:34:06 -0400

    Added ClassDie Class
    In this commit, I created the Die Class, that can set, change, and read the color and number of sides of a die.
     You can also roll the die, so see what u get

---
+++ ClassDie.py
@@ -0,0 +1,37 @@
+import random
+
+class Die(object):
+    def __init__(self, sides, color):
+        self.sides = sides
+        self.color = color
+
+    def getcolor(self):
+        return self.color
+
+    def getSides(self):
+        return self.sides
+
+    def roll(self):
+        return random.randint(1, self.sides + 1)
+
+    def changeColor(self, color):
+        self.color = color
+
+    def changeSides(self, sides):
+        self.sides = sides
+
+print("I just created a Dice Object. It has 6 sides and is red")
+rome = Die(6, "red")
+print("I rolled the dice object, and got the a ")
+number = rome.roll()
+print(number)
+
+print("I'm going to change the number of sides to 999999999 so I can roll a higher number")
+rome.changeSides(999999999)
+print("Now I'm going to Roll the dice to see what I get")
+print(rome.roll())
+
+newColor = input("Please change the color of the die from " + rome.getcolor())
+rome.changeColor(newColor)
+
+print("Congrats the new color is " + rome.getcolor())
